5a1250e6cb42e65dabc0070fe119835ca2f86a11
Serj456 <121343943+Serj456@users.noreply.github.com>
2024-12-06 16:06:34 +0300

    Update module_9_3.py

--- module_9_3.py
+++ module_9_3.py
@@ -1,7 +1,8 @@
 first = ['Strings', 'Student', 'Computers']
 second = ['Строка', 'Урбан', 'Компьютер']
 
-first_result = (len(x[0])-len(x[1]) for x in zip(first, second) if len(x[0]) != len(x[1]))
+#first_result = (len(x[0])-len(x[1]) for x in zip(first, second) if len(x[0]) != len(x[1]))
+first_result = (len(x)-len(y) for x,y in zip(first, second) if len(x) != len(y))
 print(list(first_result))
 second_result = (len(x) == len (y) for x in first for y in second if first.index(x) == second.index(y))
 print(list(second_result))
